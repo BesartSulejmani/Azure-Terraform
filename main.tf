4a530c8b32085a8d16d09d05f9a6902fb31a5bb8
BesartSulejmani <besartsulejmani@gmail.com>
2021-01-06 15:03:02 +0100

    change backend main tf file

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 # Configure the Azure provider
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "tstate"
     storage_account_name = "tstate2711"
