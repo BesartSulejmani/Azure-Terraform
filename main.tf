b6dc87570b5279223b3447be75e69ba9ba3de64b
BesartSulejmani <besartsulejmani@gmail.com>
2021-01-06 14:39:09 +0100

    removed vm in main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,13 @@ terraform {
       version = ">= 2.26"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tstate"
+    storage_account_name = "tstate2711"
+    container_name       = "tstate"
+    key                  = ".terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -79,47 +86,4 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
-}
-
-# Create a Linux virtual machine
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "myTFVM"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.myRG.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_os_disk {
-    name              = "myOsDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = lookup(var.sku, var.location)
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "myTFVM"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-data "azurerm_public_ip" "ip" {
-  name                = azurerm_public_ip.publicip.name
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  depends_on          = [azurerm_virtual_machine.vm]
-}
-
-output "public_ip_address" {
-  value = data.azurerm_public_ip.ip.ip_address
 }
